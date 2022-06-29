260257951f7280593d59d281226e10a5db5167dd
Alex Waibel <alexwaibel@users.noreply.github.com>
2022-06-29 01:31:56 +0000

    Add nicer landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import UnderConstruction from 'react-under-construction';
+import 'react-under-construction/build/css/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <UnderConstruction
+    background={{
+      image: 'https://images.unsplash.com/photo-1513331006937-db9db75ac1a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
+      textColor: '#fff',
+      overlay: {
+        color: '#000',
+        opacity: '.5'
+      }
+    }}
+    title={{
+      text: 'Money Factory'
+    }}
+    description={{
+      text: 'Our website is under construction.',
+      style: {
+        maxWidth: '440px',
+      }
+    }}
+    links={[
+      {
+        url: 'mailto:contact@moneyfactory.money',
+        image: 'https://cdn-icons-png.flaticon.com/512/2165/2165056.png',
+      },
+    ]}
+  />
+);
 
 export default App;
